03d48d63364940a4b0efdc1bb66450d521d6ed3f
EuAlmeida <jvda.vitor.almeida@gmail.com>
2022-09-06 20:04:28 -0300

    user, categoria e autor

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from core.models import livro, user, categoria
+from core.models import livro, user, categoria, autor
 
 admin.site.register(livro)
 admin.site.register(user)
 admin.site.register(categoria)
+admin.site.register(autor)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,4 @@
+from tabnanny import verbose
 from django.db import models
 
 class livro(models.Model):
@@ -27,8 +28,23 @@ class user(models.Model):
         return self.nome_usuario
 
 class categoria(models.Model):
-    id_categoria = models.AutoField(primary_key=True)
-    nome_categoria = models.CharField(max_length=30)
-    desc_categoria = models.TextField()
+    id_categoria = models.AutoField(primary_key=True, null=False)
+    nome_categoria = models.CharField(max_length=30, null=False)
+    desc_categoria = models.TextField(null=False)
+
+class autor(models.Model):
+    id_autor = models.AutoField(primary_key=True)
+    nome_autor = models.CharField(max_length=60, null=False)
+    desc_autor = models.TextField()
+    autor_nasc = models.DateField(null=False)
+    autor_falecimento = models.DateField(null=True)
+
+    def __str__(self):
+        return self.nome_autor
+    class Meta:
+        verbose_name_plural = "Autores"
+
+
+
 
 
