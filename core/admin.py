45873ec57ce9e109ff6a5c0ea34ddc6a3254ea41
EuAlmeida <jvda.vitor.almeida@gmail.com>
2022-09-06 20:16:41 -0300

    editora

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from core.models import livro, user, categoria, autor
+from core.models import livro, user, categoria, autor, editora
 
 admin.site.register(livro)
 admin.site.register(user)
 admin.site.register(categoria)
 admin.site.register(autor)
+admin.site.register(editora)
