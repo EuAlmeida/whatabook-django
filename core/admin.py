3c1004611170077e72c68fcae738bdc3fd38b120
EuAlmeida <jvda.vitor.almeida@gmail.com>
2022-09-06 19:29:41 -0300

    model-user

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from core.models import Livro
+from core.models import Livro, user
 
 admin.site.register(Livro)
+admin.site.register(user)
--- core/models.py
+++ core/models.py
@@ -2,9 +2,28 @@ from django.db import models
 
 class Livro(models.Model):
     id_livro = models.AutoField(primary_key=True)
-    sinopse_livro = models.CharField(max_length=1000)
     titulo_livro = models.CharField(max_length=100)
-    qtd_paginas = models.PositiveIntegerField()
-    ano_lancamento = models.DateField()
-    url_compra = models.URLField()
+    sinopse_livro = models.CharField(max_length=1000, null=False)
+    qtd_paginas = models.PositiveIntegerField(null=False)
+    ano_lancamento = models.DateField(null=False)
+    url_compra = models.URLField(null=False)
+    
+
+    def __str__(self):
+        return self.titulo_livro
+
+class user(models.Model):
+    id_usuario = models.AutoField(primary_key=True)
+    nome_usuario = models.CharField(max_length=60)
+    data_nasc = models.DateField(null=False)
+    telefone_user = models.PositiveIntegerField(null=False)
+    endereco_usuario = models.CharField(max_length=100, null=False)
+    cpf = models.PositiveIntegerField(null=False)
+    email_user = models.EmailField(null=False)
+    senha_user = models.CharField(max_length=20)
+    adm = models.BooleanField(null=False)
+
+    def __str__(self):
+        return self.nome_usuario
+
 
