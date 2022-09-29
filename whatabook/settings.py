90d816613e9e009e93164c906688808b11691ea3
EuAlmeida <jvda.vitor.almeida@gmail.com>
2022-09-29 13:18:58 -0300

    postgressql on railways

--- whatabook/settings.py
+++ whatabook/settings.py
@@ -68,8 +68,12 @@ WSGI_APPLICATION = "whatabook.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "railway",
+        "USER": "postgres",
+        "PASSWORD": "MYLNzfBxozcvMNFazow0",
+        "HOST": "containers-us-west-68.railway.app",
+        "PORT": "6866",
     }
 }
 
