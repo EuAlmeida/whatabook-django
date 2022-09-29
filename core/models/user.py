8597768128469a2eff1e05e74ae271050e26a9b9
EuAlmeida <jvda.vitor.almeida@gmail.com>
2022-09-29 14:17:15 -0300

    change cpf field

--- core/models/user.py
+++ core/models/user.py
@@ -1,15 +1,14 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-from cpf_field.models import CPFField
 
 
 class user(AbstractUser):
     data_nasc = models.DateField(blank=True, null=True)
     telefone_user = models.CharField(max_length=11, blank=True)
     endereco_user = models.CharField(max_length=100, blank=True)
-    cpf = CPFField("cpf",blank=True)
     foto_perfil = models.ImageField(upload_to="fotoperfil/", blank=True)   
     class Meta:
         verbose_name_plural = "Usuarios"
+
 
