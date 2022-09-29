20abefa35d26b14a98d8b48b765e82cf7a536982
EuAlmeida <jvda.vitor.almeida@gmail.com>
2022-09-29 18:40:28 -0300

    funcionando novamente?

--- whatabook/settings.py
+++ whatabook/settings.py
@@ -68,12 +68,8 @@ WSGI_APPLICATION = "whatabook.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "railway",
-        "USER": "postgres",
-        "PASSWORD": "MYLNzfBxozcvMNFazow0",
-        "HOST": "containers-us-west-68.railway.app",
-        "PORT": "6866",
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
