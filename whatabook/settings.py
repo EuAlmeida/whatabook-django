e7f3003f58755b50f3b735713261b8887cd7e15b
EuAlmeida <jvda.vitor.almeida@gmail.com>
2022-09-29 22:37:52 -0300

    primeiro teste na branch desenvolvimento

--- whatabook/settings.py
+++ whatabook/settings.py
@@ -68,8 +68,12 @@ WSGI_APPLICATION = "whatabook.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "railway",
+        "USER":"postgres",
+        "PASSWORD":"LsgOjhWbtevQsN4rPfNG",
+        "HOST":"containers-us-west-57.railway.app",
+        "PORT": "5598", 
     }
 }
 
