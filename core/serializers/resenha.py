874bd4de7300ea3ad4d2e8878cdbfc1926fd46c3
Gustavo Henrique Trentini <gustavotrentini22@gmail.com>
2022-11-22 21:53:02 -0300

    Nome do livro na resenha

--- core/serializers/resenha.py
+++ core/serializers/resenha.py
@@ -1,7 +1,7 @@
 from rest_framework.serializers import ModelSerializer
 
 from core.serializers.user import userSerializer
-from core.models import resenha
+from core.models import resenha, livro
 
 
 class resenhaSerializer(ModelSerializer):
@@ -9,8 +9,15 @@ class resenhaSerializer(ModelSerializer):
         model = resenha
         fields = "__all__"
 
+class livroSerializer(ModelSerializer):
+    resenha = resenhaSerializer(many=True)
+    class Meta:
+        model = livro
+        fields = "__all__"
+        
 class resenhadetailSerializer(ModelSerializer):
     user_resenha = userSerializer()
+    livro_resenha = livroSerializer()
     class Meta:
         model = resenha
         fields = "__all__"
