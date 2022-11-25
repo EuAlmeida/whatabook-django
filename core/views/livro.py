db04b7a8961f022bbdf847a9bb87b9b78aa036b0
Gustavo Henrique Trentini <gustavotrentini22@gmail.com>
2022-11-24 22:06:27 -0300

    Get livros por autor

--- core/views/livro.py
+++ core/views/livro.py
@@ -5,15 +5,15 @@ from core.serializers import livrodetailSerializer, livroSerializer
 
 
 class livroViewSet(ModelViewSet):
-    queryset = livro.objects.all()
+    # queryset = livro.objects.all()
     def get_serializer_class(self):
         if self.action in ['list', 'retrieve']:
             return livrodetailSerializer
         return livroSerializer
 
-    # def get_queryset(self):
-    #     queryset = livro.objects.all()
-    #     maiornota = self.request.query_params.get('nota')
-    #     if maiornota is not None:
-    #         queryset = livro.objects.filter(medianota=5).first()
-    #     return queryset
+    def get_queryset(self):
+        queryset = livro.objects.all()
+        autor = self.request.query_params.get('autor')
+        if autor is not None:
+            queryset = livro.objects.filter(autor_livros=autor)
+        return queryset
