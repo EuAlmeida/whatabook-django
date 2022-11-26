eff240dbd500dd61f7c61d543e6df08946de3fac
Gustavo Henrique Trentini <gustavotrentini22@gmail.com>
2022-11-26 17:59:32 -0300

    livros por categoria

--- core/serializers/categoria.py
+++ core/serializers/categoria.py
@@ -1,9 +1,12 @@
 from rest_framework.serializers import ModelSerializer
 
+from core.serializers.livro import livroSerializer
 from core.models import categoria
 
 
 class categoriaSerializer(ModelSerializer):
+    livros = livroSerializer
     class Meta:
         model = categoria
-        fields = "__all__"
+        fields = ("id", "nome_categoria", "livros")
+        depth = 1
